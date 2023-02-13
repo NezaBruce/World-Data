23a26127f290e1c9f785792320203b9cad52d2b9
nbrucepro <nezabruce@gmail.com>
2023-02-13 18:20:34 +0200

    [ft] - color-legen

--- src/components/pages/AxisLabel/AxisLabel.js
+++ src/components/pages/AxisLabel/AxisLabel.js
@@ -1,43 +1,43 @@
-import styled from 'styled-components';
-import PropsTypes from 'prop-types';
+// import styled from 'styled-components';
+// import PropsTypes from 'prop-types';
 
-const YAxis = styled.text`
-  transform: rotate(-90deg);
-  text-anchor: middle;
-  font-size: 2rem;
-`;
+// const YAxis = styled.text`
+//   transform: rotate(-90deg);
+//   text-anchor: middle;
+//   font-size: 2rem;
+// `;
 
-const XAxis = styled.text`
-  font-size: 2rem;
-`;
+// const XAxis = styled.text`
+//   font-size: 2rem;
+// `;
 
-function AxisLabel({
-  innerHeight,
-  innerWidth,
-  axisPadding,
-  yLabel,
-  xLabel,
-  marginLeft,
-}) {
-  return (
-    <g>
-      <YAxis y={-axisPadding} x={-innerHeight / 2}>
-        {yLabel}
-      </YAxis>
-      <XAxis y={innerHeight + axisPadding} x={innerWidth / 2 - marginLeft}>
-        {xLabel}
-      </XAxis>
-    </g>
-  );
-}
+// function AxisLabel({
+//   innerHeight,
+//   innerWidth,
+//   axisPadding,
+//   yLabel,
+//   xLabel,
+//   marginLeft,
+// }) {
+//   return (
+//     <g>
+//       <YAxis y={-axisPadding} x={-innerHeight / 2}>
+//         {yLabel}
+//       </YAxis>
+//       <XAxis y={innerHeight + axisPadding} x={innerWidth / 2 - marginLeft}>
+//         {xLabel}
+//       </XAxis>
+//     </g>
+//   );
+// }
 
-AxisLabel.propTypes = {
-  y: PropsTypes.number,
-  yLabel: PropsTypes.string,
-  xLabel: PropsTypes.string,
-  marginLeft: PropsTypes.number,
-  innerHeight: PropsTypes.number,
-  innerWidth: PropsTypes.number,
-};
+// AxisLabel.propTypes = {
+//   y: PropsTypes.number,
+//   yLabel: PropsTypes.string,
+//   xLabel: PropsTypes.string,
+//   marginLeft: PropsTypes.number,
+//   innerHeight: PropsTypes.number,
+//   innerWidth: PropsTypes.number,
+// };
 
-export default AxisLabel;
+// export default AxisLabel;
