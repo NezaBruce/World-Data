a43935a1ce2d795a0090c1e0e5ab2896ddfac271
nbrucepro <nezabruce@gmail.com>
2023-03-11 18:20:34 +0200

    [ft] - color-legen

--- src/components/utility/utility.js
+++ src/components/utility/utility.js
@@ -1,3 +1,16 @@
+// // import { format } from 'd3';
+
+// // export const formatNumber = format(',');
+// // export const formatPercentage = n => n.toFixed(2) + '%';
+
+// // export const formatString = str => {
+// //   const lowerStr = str.toLowerCase();
+// //   return lowerStr.charAt(0).toUpperCase() + lowerStr.slice(1);
+// // };
+
+// // export const width = 1000;
+// // export const height = 500;
+
 // import { format } from 'd3';
 
 // export const formatNumber = format(',');
@@ -11,16 +24,28 @@
 // export const width = 1000;
 // export const height = 500;
 
-import { format } from 'd3';
+// // import { format } from 'd3';
 
-export const formatNumber = format(',');
-export const formatPercentage = n => n.toFixed(2) + '%';
+// // export const formatNumber = format(',');
+// // export const formatPercentage = n => n.toFixed(2) + '%';
 
-export const formatString = str => {
-  const lowerStr = str.toLowerCase();
-  return lowerStr.charAt(0).toUpperCase() + lowerStr.slice(1);
-};
+// // export const formatString = str => {
+// //   const lowerStr = str.toLowerCase();
+// //   return lowerStr.charAt(0).toUpperCase() + lowerStr.slice(1);
+// // };
 
-export const width = 1000;
-export const height = 500;
+// // export const width = 1000;
+// // export const height = 500;
 
+// import { format } from 'd3';
+
+// export const formatNumber = format(',');
+// export const formatPercentage = n => n.toFixed(2) + '%';
+
+// export const formatString = str => {
+//   const lowerStr = str.toLowerCase();
+//   return lowerStr.charAt(0).toUpperCase() + lowerStr.slice(1);
+// };
+
+// export const width = 1000;
+// export const height = 500;
